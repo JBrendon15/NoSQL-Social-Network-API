dce45fa7d8ee7a53419a8580a3554104624e21a4
JBrendon15 <110281109+JBrendon15@users.noreply.github.com>
2022-10-24 02:33:02 -0500

    added function to create user

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,13 +1,27 @@
 const { User, Thought } = require('../models');
 
 module.exports = {
-    //Find all users function
+    //Find all users 
     getUsers(req, req) {
         User.find()
         .then((users) => res.json(users))
         .catch((err) => res.status(500).json(err));
     },
+    //Find one user
     getOneUser(req, req) {
         User.findOne({ _id: req.params.userId})
-    }
-}
+        .select('-__v')
+        .then((user) => {
+            !user
+            ? res.status(404).json({ message: 'No user found.'})
+            : res.json(user)
+        })
+        .catch((err) => res.status(500).json(err));
+    },
+    //Create one user
+    createUser(req, req) { 
+        User.create(req.body)
+        .then((user) => res.json(user))
+        .catch((err) => res.status(500).json(err));
+    },
+};
